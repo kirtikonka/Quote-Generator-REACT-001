bba57632a3052e77a4ec17510aa06d2ef29b7e88
Kirti Konka <107424721+kirtikonka@users.noreply.github.com>
2024-04-21 18:39:00 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = { advice: '' };
 
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = async () => { // Use async/await for cleaner error handling
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      const { advice } = response.data.slip;
+      this.setState({ advice });
+      console.log(advice);
+    } catch (error) {
+      console.error('Error fetching advice:', error);
+      // Handle errors gracefully, e.g., display an error message to the user
+    } finally {
+      // No need for logic here in most cases,
+      // but can be used for cleanup tasks if necessary
+    }
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Get Advice!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
